5e44ae7c7ea95fe1fff148045e27c55f33f35b96
Patrick Tescher <patrick@outtherelabs.com>
2018-12-06 13:24:27 -0800

    Add back http_request_tracer_from

--- actix-web-opentracing/src/lib.rs
+++ actix-web-opentracing/src/lib.rs
@@ -4,4 +4,4 @@ extern crate opentracing_rust_wip;
 
 mod http_request_tracer;
 
-pub use http_request_tracer::{HttpRequestTracer, start_child_span};
+pub use http_request_tracer::{HttpRequestTracer, start_child_span, http_request_tracer_from};
